e0ae4d45531ec6ae2ae2030cbf645bc34c40d102
Vadim Pelyushenko <vpelyush@gmail.com>
2021-03-08 21:33:36 -0800

    added manual files

---
+++ files/theme/files/script.js
@@ -0,0 +1,9 @@
+// Place Contents Here
+$(document).ready(function()
+{
+	console.log("The script is working");
+	if (!$("#navigation").length) 
+	{
+		console.log("This is the mobile version");
+	}
+});
